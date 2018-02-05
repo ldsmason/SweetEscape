20a6aaa682e253b40647bf6e9c733f5be436e5ec
chuckvale <35540577+chuckvale@users.noreply.github.com>
2018-02-05 16:58:01 -0700

    2/5/18 added Location class

--- sweetEscape/src/byui/cit260/sweetEscape/model/Location.java
+++ sweetEscape/src/byui/cit260/sweetEscape/model/Location.java
@@ -4,11 +4,58 @@
  * and open the template in the editor.
  */
 package byui.cit260.sweetEscape.model;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  *
  * @author chadh
  */
-public class Location {
+public class Location implements Serializable {
+    
+    private String row;
+    private String column;
+    private ArrayList<Game> alreadyVisited = new ArrayList<Game>();
+    private ArrayList<Game> amountRemaining = new ArrayList<Game>();
+    
+    public Location(){
+        
+    }
+
+    public String getRow() {
+        return row;
+    }
+
+    public void setRow(String row) {
+        this.row = row;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public ArrayList<Game> getAlreadyVisited() {
+        return alreadyVisited;
+    }
+
+    public void setAlreadyVisited(ArrayList<Game> alreadyVisited) {
+        this.alreadyVisited = alreadyVisited;
+    }
+
+    public ArrayList<Game> getAmountRemaining() {
+        return amountRemaining;
+    }
+
+    public void setAmountRemaining(ArrayList<Game> amountRemaining) {
+        this.amountRemaining = amountRemaining;
+    }
+    
+    
+    
     
 }
