5f4cb73ddd0bca58ae34476ae14eb1c97320831c
chuckvale <35540577+chuckvale@users.noreply.github.com>
2018-02-05 21:27:56 -0700

    2/5/18 created map class

--- sweetEscape/src/byui/cit260/sweetEscape/model/Map.java
+++ sweetEscape/src/byui/cit260/sweetEscape/model/Map.java
@@ -4,11 +4,60 @@
  * and open the template in the editor.
  */
 package byui.cit260.sweetEscape.model;
+import java.awt.Point;
+import java.io.Serializable;
 
 /**
  *
  * @author chadh
  */
-public class Map {
+public class Map implements Serializable {
     
+    Point point;
+    private String row;
+    private String column;
+    private double visited;
+    private double amountRemaining;
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public void setPoint(Point point) {
+        this.point = point;
+    }
+
+    public String getRow() {
+        return row;
+    }
+
+    public void setRow(String row) {
+        this.row = row;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public double getVisited() {
+        return visited;
+    }
+
+    public void setVisited(double visited) {
+        this.visited = visited;
+    }
+
+    public double getAmountRemaining() {
+        return amountRemaining;
+    }
+
+    public void setAmountRemaining(double amountRemaining) {
+        this.amountRemaining = amountRemaining;
+    }
+
+   
 }
