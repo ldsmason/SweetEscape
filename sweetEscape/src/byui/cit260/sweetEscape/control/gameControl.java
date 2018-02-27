20e25622c9503435f2caae047304c4cba81fc923
nathanielmason <nathanielmason@192.168.157.1>
2018-02-27 13:17:16 -0700

    2/1/18 gameMenuView and mainMenuVeiw

--- sweetEscape/src/byui/cit260/sweetEscape/control/gameControl.java
+++ sweetEscape/src/byui/cit260/sweetEscape/control/gameControl.java
@@ -4,11 +4,29 @@
  * and open the template in the editor.
  */
 package byui.cit260.sweetEscape.control;
+import byui.cit260.sweetEscape.model.*;
+import java.io.Serializable;
+import sweetescape.SweetEscape;
 
 /**
  *
  * @author nathanielmason
  */
-public class gameControl {
+public class gameControl implements Serializable {
+    
+    public static Player savePlayer(String name) {
+        Player player = new Player();
+        player.setName(name);
+        SweetEscape.setPlayer(player);
+        
+        if (name == null) {
+            return null;
+        }
+        
+        player = new Player();
+        
+       return player;     
+    }
+    
     
 }
--- sweetEscape/src/byui/cit260/sweetEscape/view/StartProgramView.java
+++ sweetEscape/src/byui/cit260/sweetEscape/view/StartProgramView.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package byui.cit260.sweetEscape.view;
+import byui.cit260.sweetEscape.model.*;
+import byui.cit260.sweetEscape.control.*;
 import java.io.Serializable;
 //import java.util.ArrayList;
 import java.util.Scanner;
@@ -26,12 +28,11 @@ public class StartProgramView implements Serializable{
                 String[] inputs = this.getInputs();
                 String value = inputs[0].toUpperCase();
                 
-                if (inputs.length < 1 || inputs.equals('Q')){
+                if (inputs.length < 1 || value.equals("Q")){
                    return; 
                 }
                 endView = doAction(inputs);
                 
-          
                 }
         while (endView != true);
         
@@ -41,6 +42,14 @@ public class StartProgramView implements Serializable{
        String[] inputs = new String[1];
        
         //Display the welcome banner
+        /*System.out.println("This adventure takes place in an abandoned warehouse in the middle "
+                + "of Central America.  User wakes up in a dark room, dazed, confused, and unaware"
+                + " how they got there.  The objective is to search the area and find a way to GET OUT."
+                + "  The user will need to locate objects and other victims in order to escape.  "
+                + "Each victim has strengths and weaknesses that will be needed to overcome specific challenges. \n" +
+                  "Along the way there will be a variety of obstacles including (but not limited to) locked doors, puzzles.  "
+                + "In order to defeat your enemies and overcome the obstacles, the user will need to invite others to join their "
+                + "party and these people will be crucial to their survival.  ");*/
        
        boolean valid = false;
        
@@ -55,8 +64,8 @@ public class StartProgramView implements Serializable{
            
            String nameWithTrim = name.trim();
            
-           if (inputs.length < 2){
-               System.out.println("You must enter a value");
+           if (inputs.length < 1){
+               System.out.println("You must enter a non-blank value");
                continue;
            }
            
@@ -71,7 +80,26 @@ public class StartProgramView implements Serializable{
     }
 
     private boolean doAction(String[] inputs) {
+       String playersName = inputs[0];
+       Player player = gameControl.savePlayer(playersName);
+       
+       if (player == null) {
+        System.out.println("Could not create the player." + 
+                          "Enter a different name.");
+        return false;
      
-    }
+        }
     
+       System.out.println("================================================="
+                        + "\nWelcome to the game " + playersName
+                        + "\nWe hope you have a lot of fun !"
+                        + "\n=================================================");
+       MainMenuView mainMenuView = new MainMenuView();
+       mainMenuView.displayMainMenuView();
+       
+       return true;
+       
+     
+
+    }
 }
