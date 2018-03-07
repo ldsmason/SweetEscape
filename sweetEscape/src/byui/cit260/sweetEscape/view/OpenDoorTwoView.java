35f967b6ada8e2f6c0707acea3d5c5f8668d8fca
chuckvale <35540577+chuckvale@users.noreply.github.com>
2018-03-07 12:57:23 -0700

    3/7/18-Door Two View

--- sweetEscape/src/byui/cit260/sweetEscape/view/OpenDoorTwoView.java
+++ sweetEscape/src/byui/cit260/sweetEscape/view/OpenDoorTwoView.java
@@ -6,6 +6,7 @@
 package byui.cit260.sweetEscape.view;
 import byui.cit260.sweetEscape.control.inventoryControl;
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  *
@@ -26,55 +27,51 @@ public class OpenDoorTwoView {
                 }
         while (endView != true);
     }
-    
+        
+        
+        
     private String[] getInputs() {
         String[] inputs = new String[1];
-        
+        Random rand = new Random();
+        int lbs = (int) (rand.nextDouble() * 10);
         boolean valid = false;
-        while (valid == false) {
-            System.out.println("Enter");
-        
-            Scanner nameInput;
-            nameInput = new Scanner(System.in);
+        while (valid == false){
             
-            String name = nameInput.nextLine();
-            
-            String nameWithTrim = name.trim();
-        }
- 
-  
-        Scanner fillBottle = new Scanner(System.in);
-        System.out.println("Please enter bottle size.");
-        Scanner inFile;
-        inFile= new Scanner(System.in);
+            System.out.println("There is a number on the wall which reads " + lbs + " lbs. "
+                + "\n You also see a scale and can see that buy placing a weight that"
+                + "\n matches the number on the wall, you can open the door.  Using"
+                + "\n the 2-liter bottle and the sand in your inventory,  determine"
+                + "\n the number of times needed to fill the bottle in order to open"
+                + "\n the door.  The ratio is 2.2 L/lb. \n\n Please enter the number of times needed to fill"
+                + "\n bottle with sand. ");
        
-        while (valid==false) {
-           System.out.println("Please enter the number of times needed to fill"
-                             + "\n bottle with sand.");
-           Scanner nameInput;
-           nameInput = new Scanner(System.in);
-           
-           String name = nameInput.nextLine();
-           
-           String nameWithTrim = name.trim();
-           
-           if (inputs.length < 1) {
-               System.out.println("You must enter a value");
-               continue;   
+            Scanner inFile = new Scanner(System.in);
+            double guess = inFile.nextDouble();
+            if (inputs.length < 1) {
+                System.out.println("You must enter a value");      
             }
-           
-           else {
-               inputs[0] = nameWithTrim;
-               valid = true;
-           }  
+            else {
+                inputs[0] = Double.toString(guess);
+                inputs[1] = Double.toString(lbs);
+                valid = true;
+            }           
         }
+        
         return inputs;
     }
 
     private boolean doAction(String[] inputs) {
        String menuItem = inputs[0];
        menuItem = inputs[0].toUpperCase();
-       
+           /*
+          double liters = inventoryControl.calcLiters(guess, answer);
+               if (liters >= 1 || liters <= 50) {
+                   break;
+               }
+               if (liters >= 50 || liters <=1) {
+                   System.out.println("try again");
+               }
+           } */ 
        switch (menuItem) {
            case "Enter" : calcLiters();
            break;
